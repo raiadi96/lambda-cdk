9d516b7e97c9f3872488de57bd7aa07d4a88a6e8
Aditya Rai <kaditya1298@gmail.com>
2023-04-23 02:55:49 +0530

    a basic cdk with lambda-api gateway integration

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +8,18 @@ export class LambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambda = new Function(
+      this,
+      'LambdaCdkStackLambda',
+      {
+        runtime: Runtime.NODEJS_14_X,
+        code: Code.fromAsset('lambda'),
+        handler: 'hello.handler',
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new LambdaRestApi(this, 'LambdaCdkStackApi', {
+    handler: lambda,
+    });
   }
 }
